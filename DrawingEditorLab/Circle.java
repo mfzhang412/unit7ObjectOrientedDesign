22fa48ab36d9e33421c555ca89ea454f1b6d7b15
mfzhang412 <mfzhang@NN123-HA-09.HS.sd203.org>
2016-03-01 10:40:00 -0600

    Lab 6

--- DrawingEditorLab/Circle.java
+++ DrawingEditorLab/Circle.java
@@ -1,4 +1,7 @@
-
+import java.awt.geom.Point2D;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 /**
  * Write a description of class Circle here.
@@ -7,9 +10,43 @@
  * @version (a version number or a date)
  */
 public class Circle extends Shape
-{
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
-
+{    
+    public Circle(Point2D.Double c, double r, Color cC)
+    {
+        super(c, r, cC);
+    }
     
+    public boolean isInside(Point2D.Double point)
+    {
+        boolean tf = false;
+        double x = point.getX();
+        double y = point.getY();
+        if (((x > (this.getCenter()).getX() - this.getRadius()) && (x < (this.getCenter()).getX() + this.getRadius())) &&
+            ((y > (this.getCenter()).getY() - this.getRadius()) && (y < (this.getCenter()).getY() + this.getRadius())))
+            {
+                tf = true;
+            }
+        
+        return tf;
+    }
+    
+    public boolean isOnBorder(Point2D.Double point)
+    {
+        boolean tf = false;
+        double x = point.getX();
+        double y = point.getY();
+        
+        //if (((x = (this.getCenter()).getX() - this.getRadius()) || (x < (this.getCenter()).getX() + this.getRadius())) &&
+            ((y > (this.getCenter()).getY() - this.getRadius()) && (y < (this.getCenter()).getY() + this.getRadius())))
+            {
+                tf = true;
+            }
+        
+        return tf;
+    }
+    
+    public void draw(Graphics2D g2, boolean filled)
+    {
+        
+    }
 }
--- DrawingEditorLab/Shape.java
+++ DrawingEditorLab/Shape.java
@@ -1,9 +1,7 @@
-import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 
 /**
  * Write a description of abstract class Shape here.
@@ -16,45 +14,37 @@ abstract public class Shape
     Point2D.Double center;
     double radius;
     Color color;
-    public Shape(Point2D.Double center, double radius, Color color)
+    
+    public Shape(Point2D.Double c, double r, Color cC)
     {
-        this.center = center;
-        this.radius = radius;
-        this.color = color;
+        center = c;
+        radius = r;
+        color = cC;
     }
     
     public Point2D.Double getCenter()
     {
-        
+        return center;
     }
     
     public double getRadius()
     {
-        
+        return radius;
     }
     
     public void move(double x, double y)
     {
-        
+        center.setLocation(x, y);
     }
     
     public void setRadius(double r)
     {
-        
+        radius = r;
     }
     
-    public boolean isInside(Point2D.Double point)
-    {
-        
-    }
+    abstract public boolean isInside(Point2D.Double point);
     
-    public boolean isOnBorder(Point2D.Double point)
-    {
-        
-    }
+    abstract public boolean isOnBorder(Point2D.Double point);
     
-    public void draw(Graphics2D g2, boolean filled)
-    {
-        
-    }
+    abstract public void draw(Graphics2D g2, boolean filled);
 }
